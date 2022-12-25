f063e7aecb341c4b5f0dca407cfb41c221fce4f5
Tomas Cuevas <memo_mister398@hotmail.com>
2022-12-25 16:52:10 -0300

    chore: :bento: add pictures and _document file

---
+++ pages/_document.tsx
@@ -0,0 +1,35 @@
+import Document, {
+  DocumentContext,
+  Html,
+  Head,
+  Main,
+  NextScript,
+} from "next/document";
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initalProps = await Document.getInitialProps(ctx);
+
+    return initalProps;
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&display=swap"
+            rel="stylesheet"
+          />
+          <link rel="icon" href="/t-logo.png" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
