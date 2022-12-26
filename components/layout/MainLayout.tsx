ed21067586b3b5ee4a215432e5852e78b03f25f0
Tomas Cuevas <memo_mister398@hotmail.com>
2022-12-26 10:50:03 -0300

    feat: :sparkles: add Main Layout

---
+++ components/layout/MainLayout.tsx
@@ -0,0 +1,36 @@
+import { useContext } from "react";
+import Head from "next/head";
+
+//* components *//
+import { Header } from "../header";
+import { Sidebar } from "../sidebar";
+
+//* context *//
+import { UIContext } from "../../context";
+
+//* interface *//
+interface Props {
+  children: React.ReactNode;
+  description: string;
+  title: string;
+}
+
+export const MainLayout: React.FC<Props> = ({
+  children,
+  description,
+  title,
+}) => {
+  const { isSidebarOpen } = useContext(UIContext);
+
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta name="description" content={description} />
+      </Head>
+      {isSidebarOpen ? <Sidebar /> : null}
+      <Header />
+      <main className="bg-orange min-h-[calc(100vh_-_80px)]">{children}</main>
+    </>
+  );
+};
