c6e6c3047554e495c3eec3f3738d5864078b0e7a
Tomas Cuevas <memo_mister398@hotmail.com>
2022-12-29 21:25:51 -0300

    style: :dizzy: add animation on Header component

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,4 +1,5 @@
 import { useContext } from "react";
+import { motion, Variants } from "framer-motion";
 
 //* icons *//
 import { MdMenu } from "react-icons/md";
@@ -6,18 +7,58 @@ import { MdMenu } from "react-icons/md";
 //* context *//
 import { UIContext } from "../../context";
 
+const headerAnimation: Variants = {
+  offscreen: { opacity: 0 },
+  onscreen: { opacity: 1 },
+};
+
 export const Header: React.FC = () => {
   const { toggleSidebar } = useContext(UIContext);
 
   return (
-    <header className="flex h-20 bg-orange px-4 justify-between sticky top-0 left-0 shadow-2xl z-20">
-      <img src="/t-logo.png" alt="logo" className="h-full" />
-      <button type="button">
+    <motion.header
+      initial="offscreen"
+      whileInView="onscreen"
+      variants={headerAnimation}
+      viewport={{ amount: 0.1, once: true }}
+      transition={{ duration: 0.5, delay: 2 }}
+      className="fixed top-0 left-0 z-20 flex h-20 w-full justify-between bg-dark/20 px-4 shadow-2xl shadow-purple/20 backdrop-blur-lg xs:px-10 lg:px-14 xl:px-24"
+    >
+      <a href="#top">
+        <img src="/t-logo.png" alt="logo" className="h-full" />
+      </a>
+      <button type="button" className="md:hidden">
         <MdMenu
           onClick={toggleSidebar}
-          className="h-10 w-10 text-red relative z-50"
+          className="relative z-50 h-10 w-10 text-purple"
         />
       </button>
-    </header>
+      <ul className="hidden items-center gap-8 md:flex">
+        <li>
+          <a
+            href="#top"
+            className="text-xl font-light tracking-[1px] text-light/60 transition-all duration-300 hover:text-light"
+          >
+            Inicio
+          </a>
+        </li>
+        <li>
+          <a
+            href="#about_me"
+            className="text-xl font-light tracking-[1px] text-light/60 transition-all duration-300 hover:text-light"
+          >
+            Acerca de
+          </a>
+        </li>
+        <li>
+          <a
+            href="#projects"
+            className="text-xl font-light tracking-[1px] text-light/60 transition-all duration-300 hover:text-light"
+          >
+            Proyectos
+          </a>
+        </li>
+      </ul>
+    </motion.header>
   );
 };
