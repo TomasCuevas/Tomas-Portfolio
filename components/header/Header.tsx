9ac93ff2b61e0e1238746c6fc47db46aa9fc056a
Tomas Cuevas <memo_mister398@hotmail.com>
2023-01-23 15:50:35 -0300

    style: :lipstick: change styles on header and sidebar

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -22,52 +22,65 @@ export const Header: React.FC = () => {
       variants={headerAnimation}
       viewport={{ amount: 0.1, once: true }}
       transition={{ duration: 0.5, delay: 2 }}
-      className="fixed top-0 left-0 z-20 flex h-20 w-full justify-between bg-dark/20 px-4 shadow-2xl shadow-purple/20 backdrop-blur-lg xs:px-10 lg:px-14 xl:px-24"
+      className="fixed top-0 left-0 z-20 flex h-[66px] w-full items-end justify-center bg-[#0000] px-4 backdrop-blur-sm xs:px-10 md:h-20 md:px-16 lg:px-8 xl:px-0"
     >
-      <a href="#top">
-        <img
-          src="/t-logo.png"
-          alt="logo"
-          className="h-full opacity-80 transition-all duration-300 hover:opacity-100"
-        />
-      </a>
-      <button type="button" className="md:hidden">
-        <MdMenu
-          onClick={toggleSidebar}
-          className="relative z-50 h-10 w-10 text-purple/80 transition-all duration-300 hover:text-purple"
-        />
-      </button>
-      <ul className="hidden items-center gap-8 md:flex">
-        <li>
+      <div className="flex h-14 w-full max-w-[1000px] items-center justify-between rounded-full bg-darkLight/90 px-4 backdrop-blur-sm md:h-[66px] md:px-8">
+        <a href="#top" className="h-full w-[95px]">
+          <img
+            src="/t-logo.png"
+            alt="logo"
+            className="h-full opacity-80 transition-all duration-300 hover:opacity-100"
+          />
+        </a>
+        <button type="button" className="md:hidden">
+          <MdMenu
+            onClick={toggleSidebar}
+            className="relative z-50 h-10 w-10 text-purple transition-all duration-300"
+          />
+        </button>
+        <ul className="hidden h-full items-center gap-5 md:flex lg:gap-7">
+          <li>
+            <a
+              href="#top"
+              className="text-xl font-light tracking-[1px] text-light/60 transition-all duration-300 hover:text-light"
+            >
+              Inicio
+            </a>
+          </li>
+          <li>
+            <a
+              href="#about_me"
+              className="text-xl font-light tracking-[1px] text-light/60 transition-all duration-300 hover:text-light"
+            >
+              Acerca de
+            </a>
+          </li>
+          <li>
+            <a
+              href="#projects"
+              className="text-xl font-light tracking-[1px] text-light/60 transition-all duration-300 hover:text-light"
+            >
+              Proyectos
+            </a>
+          </li>
+          <li>
+            <a
+              href="#skills"
+              className="text-xl font-light tracking-[1px] text-light/60 transition-all duration-300 hover:text-light"
+            >
+              Habilidades
+            </a>
+          </li>
+        </ul>
+        <div className="hidden items-center rounded-md border border-light/60 text-light/60 transition-all duration-300 hover:border-light hover:text-light md:flex">
           <a
-            href="#top"
-            className="text-xl font-light tracking-[1px] text-light/60 transition-all duration-300 hover:text-light"
+            href="#contact"
+            className="py-1 px-3 text-xl font-light tracking-[1px]"
           >
-            Inicio
-          </a>
-        </li>
-        <li>
-          <a
-            href="#about_me"
-            className="text-xl font-light tracking-[1px] text-light/60 transition-all duration-300 hover:text-light"
-          >
-            Acerca de
-          </a>
-        </li>
-        <li>
-          <a
-            href="#projects"
-            className="text-xl font-light tracking-[1px] text-light/60 transition-all duration-300 hover:text-light"
-          >
-            Proyectos
-          </a>
-        </li>
-        <li className="rounded-md border border-light/60 px-3 py-1 text-light/60 transition-all duration-300 hover:border-light hover:text-light">
-          <a href="#contact" className="text-xl font-light tracking-[1px]">
             Contacto
           </a>
-        </li>
-      </ul>
+        </div>
+      </div>
     </motion.header>
   );
 };
--- components/sidebar/Sidebar.tsx
+++ components/sidebar/Sidebar.tsx
@@ -54,7 +54,7 @@ export const Sidebar = () => {
               animate="onscreen"
               variants={contactAnimation}
               onClick={toggleSidebar}
-              className="w-full rounded-md border border-light/80  text-center text-2xl text-light/80 transition-all duration-300 hover:border-light hover:text-light"
+              className="w-full rounded-md border border-light/80 text-center text-2xl text-light/80 transition-all duration-300 hover:border-light hover:text-light"
             >
               <a
                 href="#contact"
@@ -109,6 +109,26 @@ export const Sidebar = () => {
                 className="flex w-full pb-2 text-light/60 transition-all duration-300 hover:text-light"
               >
                 Proyectos
+              </motion.a>
+              <motion.div
+                initial="offscreen"
+                animate="onscreen"
+                variants={borderAnimation}
+                className="h-[1px] w-full bg-purple"
+              ></motion.div>
+            </li>
+            <li
+              onClick={toggleSidebar}
+              className="w-full border-b border-purple text-2xl"
+            >
+              <motion.a
+                initial="offscreen"
+                animate="onscreen"
+                variants={textAnimation}
+                href="#skills"
+                className="flex w-full pb-2 text-light/60 transition-all duration-300 hover:text-light"
+              >
+                Habilidades
               </motion.a>
               <motion.div
                 initial="offscreen"
