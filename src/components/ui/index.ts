ece20c5f0be5bcab8fe2e8176779f36fcc9a7561
Tomas Cuevas <memo_mister398@hotmail.com>
2023-07-14 16:01:58 -0300

    feat: :sparkles: add BottomWave component

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,4 +1,7 @@
+export * from "./Alert/Alert";
+export * from "./BottomWave/BottomWave";
 export * from "./SectionTitle/SectionTitle";
 export * from "./SectionXHoc/SectionXHoc";
 export * from "./SectionYHoc/SectionYHoc";
 export * from "./SkillsFeedHoc/SkillsFeedHoc";
+export * from "./ThemeSwitch/ThemeSwitch";
